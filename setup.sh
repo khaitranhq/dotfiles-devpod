a47241f8675469bd68babaa7fc560b9ce40c247a
Khai Tran <khaitran.lewis@gmail.com>
2024-06-28 22:35:40 +0700

    fix: Update fish installation method

--- setup.sh
+++ setup.sh
@@ -9,12 +9,9 @@ ln -sf "$PWD/lazygit" "$XDG_CONFIG_HOME"/lazygit
 cp -r "$PWD/fish" "$XDG_CONFIG_HOME"
 cp -r "$PWD/ohmyposh" "$XDG_CONFIG_HOME"
 
-sudo apt update
-sudo apt install software-properties-common -y
-
-
 # Install fish
-sudo apt-add-repository ppa:fish-shell/release-3
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
 sudo apt update
 sudo apt install fish
 
