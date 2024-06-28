6a60af7c2138694ac8df0e90c4abc066ab67c3a8
Khai Tran <khaitran.lewis@gmail.com>
2024-06-28 21:20:05 +0700

    feat: Add fish shell configuration and oh-my-posh theme

--- setup.sh
+++ setup.sh
@@ -6,13 +6,22 @@ mkdir -p "$XDG_CONFIG_HOME"
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 ln -sf "$PWD/lazygit" "$XDG_CONFIG_HOME"/lazygit
+cp -r "$PWD/fish" "$XDG_CONFIG_HOME"
+cp -r "$PWD/ohmyposh" "$HOME"/.local/share
 
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+sudo apt update
 sudo apt install build-essential -y
-brew -v
+
+if ! command -v brew -v &> /dev/null
+then
+    echo "Install brew failed"
+    exit 1
+fi
 
 packages=(
 	fd
@@ -22,7 +31,7 @@ packages=(
 	kubectl
   neovim
   zoxide
-  # fish
+  fish
   # oh-my-posh
 )
 
@@ -30,5 +39,9 @@ for package in "${packages[@]}"; do
 	echo "Installing $package..."
 	brew install "$package"
 done
+
+# Change default shell to fish
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
 
 echo "All packages from the setup script have been installed."
