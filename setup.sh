e4c36cf72d94d272a0345f6b6717858abe333c59
Khai Tran <khaitran.lewis@gmail.com>
2024-06-28 22:01:34 +0700

    refactor: Update setup script to install fish shell

--- setup.sh
+++ setup.sh
@@ -9,43 +9,52 @@ ln -sf "$PWD/lazygit" "$XDG_CONFIG_HOME"/lazygit
 cp -r "$PWD/fish" "$XDG_CONFIG_HOME"
 cp -r "$PWD/ohmyposh" "$XDG_CONFIG_HOME"
 
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-echo 'export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin' | sudo tee -a /etc/environment
-
+# Install fish
+sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt install build-essential -y
-
-if ! command -v brew -v &> /dev/null
-then
-    echo "Install brew failed"
-    exit 1
-fi
-
-packages=(
-	# fd
-	# ripgrep
-	# npm
-	# lazygit
-	# kubectl
- #  neovim
- #  zoxide
-  fish
-  # oh-my-posh
-)
-
-for package in "${packages[@]}"; do
-	echo "Installing $package..."
-	brew install "$package"
-done
-
-# Change default shell to fish
+sudo apt install fish -y
 fish_directory=$(which fish)
-echo $fish_directory | sudo tee -a /etc/shells
 echo $fish_directory
-# sudo chsh -s $fish_directory
+echo $fish_directory | sudo tee -a /etc/shells
+sudo chsh -s $fish_directory
 
-echo "All packages from the setup script have been installed."
+
+# # Install homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
+#
+# sudo apt update
+# sudo apt install build-essential -y
+#
+# if ! command -v brew -v &> /dev/null
+# then
+#     echo "Install brew failed"
+#     exit 1
+# fi
+#
+# packages=(
+# 	# fd
+# 	# ripgrep
+# 	# npm
+# 	# lazygit
+# 	# kubectl
+#  #  neovim
+#  #  zoxide
+#   fish
+#   # oh-my-posh
+# )
+#
+# for package in "${packages[@]}"; do
+# 	echo "Installing $package..."
+# 	brew install "$package"
+# done
+#
+# source "$HOME"/.bashrc
+#
+# # Change default shell to fish
+# fish_directory=$(which fish)
+# echo $fish_directory | sudo tee -a /etc/shells
+# echo $fish_directory
+# sudo chsh -s $fish_directory
+#
+# echo "All packages from the setup script have been installed."
