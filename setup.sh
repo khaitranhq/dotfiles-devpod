1736ee2c7e095aac92986d7c47c60fa464db2fbc
Khai Tran <khaitran.lewis@gmail.com>
2024-06-28 22:37:07 +0700

    fix: Update fish installation command with -y flag

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ cp -r "$PWD/ohmyposh" "$XDG_CONFIG_HOME"
 echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
 curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
 sudo apt update
-sudo apt install fish
+sudo apt install fish -y
 
 fish_directory=$(which fish)
 echo $fish_directory | sudo tee -a /etc/shells
