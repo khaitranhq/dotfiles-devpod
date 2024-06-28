d7bdb231d866153c846b1b323648ef2b326e286d
Khai Tran <khaitran.lewis@gmail.com>
2024-06-28 21:02:00 +0700

    feat: Add lazygit configuration and setup script updates

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@ export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
-
 ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
+ln -sf "$PWD/lazygit" "$XDG_CONFIG_HOME"/lazygit
 
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt install build-essential -y
-brew -h
+brew -v
 
 packages=(
 	fd
@@ -21,6 +21,9 @@ packages=(
 	lazygit
 	kubectl
   neovim
+  zoxide
+  # fish
+  # oh-my-posh
 )
 
 for package in "${packages[@]}"; do
