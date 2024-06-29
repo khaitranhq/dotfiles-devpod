1af09388bf52a26ec4e687a186babe4ffccfdacf
Khai Tran <khaitran.lewis@gmail.com>
2024-06-29 10:27:00 +0700

    refactor: Update setup.sh script for homebrew installation

--- setup.sh
+++ setup.sh
@@ -3,50 +3,50 @@
 export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
-
-source "$HOME"/.bashrc
-
 sudo apt update
 sudo apt install build-essential -y
 
-if ! command -v brew -v &> /dev/null
-then
-    echo "Install brew failed"
-    exit 1
-fi
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
 
-# Install packages
-packages=(
-	fd
-	ripgrep
-  neovim
-	npm
-	lazygit
-  fish
-  exa
-  fzf
-  zoxide
-)
+source "$HOME"/.bashrc
 
-for package in "${packages[@]}"; do
-	echo "Installing $package..."
-	brew install "$package"
-done
-
-fish_directory=$(which fish)
-echo $fish_directory | sudo tee -a /etc/shells
-sudo chsh -s $fish_directory
-sudo sed -i '$s/.*/node:x:1000:1000::\/home\/node:\/usr\/bin\/fish/' /etc/passwd
-
-
-ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
-ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
-ln -sf "$PWD/lazygit" "$XDG_CONFIG_HOME"/lazygit
-cp -r "$PWD/fish" "$XDG_CONFIG_HOME"
-cp -r "$PWD/ohmyposh" "$XDG_CONFIG_HOME"
+# if ! command -v brew -v &> /dev/null
+# then
+#     echo "Install brew failed"
+#     exit 1
+# fi
+#
+# # Install packages
+# packages=(
+# 	fd
+# 	ripgrep
+#   neovim
+# 	npm
+# 	lazygit
+#   fish
+#   exa
+#   fzf
+#   zoxide
+# )
+#
+# for package in "${packages[@]}"; do
+# 	echo "Installing $package..."
+# 	brew install "$package"
+# done
+#
+# fish_directory=$(which fish)
+# echo $fish_directory | sudo tee -a /etc/shells
+# sudo chsh -s $fish_directory
+# sudo sed -i '$s/.*/node:x:1000:1000::\/home\/node:\/usr\/bin\/fish/' /etc/passwd
+#
+#
+# ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+# ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
+# ln -sf "$PWD/lazygit" "$XDG_CONFIG_HOME"/lazygit
+# cp -r "$PWD/fish" "$XDG_CONFIG_HOME"
+# cp -r "$PWD/ohmyposh" "$XDG_CONFIG_HOME"
 
 exit 0
 #
