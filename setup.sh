a92480d32c276a653a9efd0c4335866503040940
Khai Tran <khaitran.lewis@gmail.com>
2024-06-30 15:31:20 +0700

    refactor: Simplify setup script by removing redundant comments

--- setup.sh
+++ setup.sh
@@ -14,47 +14,47 @@ echo $fish_directory | sudo tee -a /etc/shells
 sudo chsh -s $fish_directory
 sudo sed -i '$s/.*/node:x:1000:1000::\/home\/node:\/usr\/bin\/fish/' /etc/passwd
 
-# Install homebrew
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sudo /bin/bash
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-source "$HOME"/.bashrc
-
-if ! command -v brew -v &> /dev/null
-then
-    echo "Install brew failed"
-    exit 1
-fi
-
-# Install packages
-packages=(
-	fd
-	ripgrep
-  neovim
-	npm
-	lazygit
-  jandedobbeleer/oh-my-posh/oh-my-posh
-  fzf
-  zoxide
-  tmux
-  nvm
-)
-
-for package in "${packages[@]}"; do
-	echo "Installing $package..."
-	brew install "$package"
-done
-
-TMUX_PLUGIN_MANAGER_PATH="$HOME"/.tmux/plugins
-mkdir -p $TMUX_PLUGIN_MANAGER_PATH
-git clone https://github.com/tmux-plugins/tpm "$TMUX_PLUGIN_MANAGER_PATH"/tpm
-
-ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
-ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
-ln -sf "$PWD/lazygit" "$XDG_CONFIG_HOME"/lazygit
-cp -r "$PWD/fish" "$XDG_CONFIG_HOME"
-cp -r "$PWD/ohmyposh" "$XDG_CONFIG_HOME"
-
-
-echo "All packages from the setup script have been installed."
-exit 0
+# # Install homebrew
+# curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sudo /bin/bash
+# (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# source "$HOME"/.bashrc
+#
+# if ! command -v brew -v &> /dev/null
+# then
+#     echo "Install brew failed"
+#     exit 1
+# fi
+#
+# # Install packages
+# packages=(
+# 	fd
+# 	ripgrep
+#   neovim
+# 	npm
+# 	lazygit
+#   jandedobbeleer/oh-my-posh/oh-my-posh
+#   fzf
+#   zoxide
+#   tmux
+#   nvm
+# )
+#
+# for package in "${packages[@]}"; do
+# 	echo "Installing $package..."
+# 	brew install "$package"
+# done
+#
+# TMUX_PLUGIN_MANAGER_PATH="$HOME"/.tmux/plugins
+# mkdir -p $TMUX_PLUGIN_MANAGER_PATH
+# git clone https://github.com/tmux-plugins/tpm "$TMUX_PLUGIN_MANAGER_PATH"/tpm
+#
+# ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+# ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
+# ln -sf "$PWD/lazygit" "$XDG_CONFIG_HOME"/lazygit
+# cp -r "$PWD/fish" "$XDG_CONFIG_HOME"
+# cp -r "$PWD/ohmyposh" "$XDG_CONFIG_HOME"
+#
+#
+# echo "All packages from the setup script have been installed."
+# exit 0
