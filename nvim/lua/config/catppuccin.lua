b8e919f2cad16b55f5bb11a671605985a230da59
Khai Tran <khaitran.lewis@gmail.com>
2024-07-05 10:42:49 +0700

    refactor: Update dependencies and configurations

--- nvim/lua/config/catppuccin.lua
+++ nvim/lua/config/catppuccin.lua
@@ -6,7 +6,7 @@ return {
         config = function()
             require("catppuccin").setup(
                 {
-                    flavour = "mocha"
+                    flavour = "macchiato"
                 }
             )
             vim.cmd.colorscheme "catppuccin"
--- nvim/lua/config/mason.lua
+++ nvim/lua/config/mason.lua
@@ -5,19 +5,7 @@ return {
 		config = function()
 			require("mason").setup()
 			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"pyright",
-					"tsserver",
-					"gopls",
-					"clangd",
-					"bashls",
-					"cssls",
-					"jsonls",
-					"dockerls",
-					"docker_compose_language_service",
-					"rust_analyzer",
-				},
+				automatic_installation = true,
 			})
 		end,
 	},
@@ -28,18 +16,17 @@ return {
 
 		config = function()
 			require("mason-tool-installer").setup({
-
 				-- a list of all tools you want to ensure are installed upon
 				-- start
 				ensure_installed = {
 					"stylua",
-          "golines",
-          "prettier",
-          "shfmt",
-          "black",
-          "clang-format",
-          "yamlfmt",
-          "rustfmt"
+					"golines",
+					"prettier",
+					"shfmt",
+					"black",
+					"clang-format",
+					"yamlfmt",
+					"rustfmt",
 				},
 			})
 		end,
